f71bb09c898f48f29381012081f467e8d326f5e8
zhangpan <zhangpanrobot@gmail.com>
2022-06-07 02:44:19 +0800

    update eslint

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,16 +1,98 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import resolve, { lib2esm } from 'vite-plugin-resolve'
 import electron from 'vite-plugin-electron/renderer'
 import pkg from '../../package.json'
+import path from 'path'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import eslintPlugin from 'vite-plugin-eslint'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vitejs.dev/config/
+const envStr = 'test'
+
+const evnPath = `${envStr}`
+
+const pathMap = {
+  online: '',
+  test: evnPath,
+  pre: evnPath,
+}
+
 export default defineConfig({
-  mode: process.env.NODE_ENV,
-  root: __dirname,
+  base: './',
+  build: {
+    outDir: '../../dist/renderer',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, './src')}/`,
+    },
+  },
+  server: {
+    host: pkg.env.VITE_DEV_SERVER_HOST,
+    port: pkg.env.VITE_DEV_SERVER_PORT,
+    strictPort: false,
+    proxy: {
+      '/buscustomized': {
+        target: pathMap[envStr],
+        rewrite: (path) => path.replace(/^\/buscustomized/, '/customized_busd/buscustomized/'),
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+    hmr: {
+      overlay: true,
+    },
+  },
   plugins: [
     vue(),
     electron(),
+    vueJsx(),
+    Components({
+      dts: true,
+      dirs: ['src/components'],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass',
+        }),
+      ],
+    }),
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), 'src/icons')],
+      // Specify symbolId format
+      symbolId: 'icon-[dir]-[name]',
+    }),
+    eslintPlugin(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+        /\.json$/, // .json
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+      ],
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+      resolvers: [() => null],
+      dts: './auto-imports.d.ts',
+    }),
     resolve(
       /**
        * Here you can specify other modules
@@ -46,14 +128,9 @@ export default defineConfig({
       }
     ),
   ],
-  base: './',
-  build: {
-    outDir: '../../dist/renderer',
-    emptyOutDir: true,
-    sourcemap: true,
-  },
-  server: {
-    host: pkg.env.VITE_DEV_SERVER_HOST,
-    port: pkg.env.VITE_DEV_SERVER_PORT,
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
   },
 })
